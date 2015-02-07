b9ea6123cdf688b8baac92cccd56bd2dc481dc50
John-Michael Reed <JohnReedLOL@ufl.edu>
2015-02-07 00:58:14 -0500

    MapMain_Association uml

---
+++ src/view/StatsView (John-Michael Reed's conflicted copy 2015-02-06).java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.view;
+
+import src.controller.Avatar;
+import src.controller.Entity;
+/**
+ * Players see the StatsView when they are checking their stats
+ * @author JohnReedLOL
+ */
+final class StatsView extends Viewport
+{
+    // Converts the class name into a base 35 number
+    private static final long serialVersionUID = Long.parseLong("StatsView", 35);
+	
+
+    private final Avatar avatar_reference_;
+
+    /**
+     * Generates a new StatsView using the avatar_reference.
+     */
+    public StatsView(Avatar my_avatar) {
+    	avatar_reference_ = my_avatar;
+    }
+}
